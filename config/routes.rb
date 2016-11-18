c9e8d286646180c72f6d95af17ba668f37b0a1e7
Michael OConnor <oconnor@my.ccsu.edu>
2016-11-18 17:49:03 +0000

    Adding static pgs controller and root route to /home

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+    
+  get 'static_pages/home'
+
   resources :species
   get 'sightings/new'
 
   resources :sightings
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'application#hello'
 end
