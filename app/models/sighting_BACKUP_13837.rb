0eb6486c036a88359511a1b9e31fe0988f1f0fa8
Michael OConnor <oconnor@my.ccsu.edu>
2016-12-02 22:12:26 +0000

    This file is breaking heroku

--- app/models/sighting_BACKUP_13837.rb
+++
@@ -1,9 +0,0 @@
-class Sighting < ApplicationRecord
-<<<<<<< HEAD
-    validates :user, :name, :description, :presence=>true
-    #belongs_to:species
-=======
-  validates :user, :name, :description, :presence=>true
-  belongs_to :species, :class_name => "Species"
->>>>>>> 0af7b5e2cb60a5d2217f25236fd21ad01340ced6
-end
