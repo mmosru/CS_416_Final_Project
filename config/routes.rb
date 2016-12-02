788574a4525148f0101299fe1c5302449ee9bca7
Michael OConnor <oconnor@my.ccsu.edu>
2016-12-02 00:47:26 +0000

    basic layout for signup/signin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+
   resources :users
   root 'static_pages#home'
     
@@ -11,4 +13,21 @@ Rails.application.routes.draw do
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+end
+
+Rails.application.routes.draw do
+  root   'static_pages#home'
+
+  get    '/signup',  to: 'users#new'
+  get    '/signin',  to: 'sessions#new'
+  post   '/signin',  to: 'sessions#create'
+  delete '/signout', to: 'sessions#destroy'
+  resources :users
+  
+  get 'static_pages/home'
+
+  resources :species
+  get 'sightings/new'
+
+  resources :sightings
 end
