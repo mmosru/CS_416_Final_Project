1802783f41fe21b1fdd6da5783902e4abe59ebac
Uka <chukwuogor@my.ccsu.edu>
2016-12-03 01:19:47 +0000

    partial header added

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,14 @@
 
 ActiveRecord::Schema.define(version: 20161202010130) do
 
+create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
+  end
+  
   create_table "sightings", force: :cascade do |t|
     t.string   "location"
     t.datetime "observe_tm"
@@ -33,12 +41,6 @@ ActiveRecord::Schema.define(version: 20161202010130) do
     t.datetime "updated_at",    null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.string   "password_digest"
-  end
+  
 
 end
