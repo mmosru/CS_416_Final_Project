54af2da78280623e582d3b5a48c530ca0d23dffc
Michael OConnor <oconnor@my.ccsu.edu>
2016-12-03 17:28:53 +0000

    Adding bootstrap libs and custom css file

--- db/schema.rb
+++ db/schema.rb
@@ -12,14 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20161202010130) do
 
-create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.string   "password_digest"
-  end
-  
   create_table "sightings", force: :cascade do |t|
     t.string   "location"
     t.datetime "observe_tm"
@@ -41,6 +33,12 @@ create_table "users", force: :cascade do |t|
     t.datetime "updated_at",    null: false
   end
 
-  
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
+  end
 
 end
